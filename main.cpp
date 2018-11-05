b8b9128118f4c277600a12fa03f382a97983c8c7
Kyle Curry <kylecurry1990@gmail.com>
2018-11-05 12:03:33 -0600

    updating main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,129 @@
-/**
-*	@author 
-*	@date 
-*	@file main.cpp
-*	@brief driver for LinkedList demo
-*/
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "LinkedListOfInts.h"
 
-int main(int argc, char** argv)
+//Testing isEmpty function
+TEST_CASE("An empty list reports empty", "[List]")
 {
-	//Example of declaring a LinkedListOfInts
-	LinkedListOfInts testableList;
+  LinkedListOfInts list;
+  SECTION("list is empty")
+  {
+    CHECK(list.isEmpty() == true);
+  }
+  SECTION("list is not empty")
+  {
+    list.addFront(1);
+    CHECK(list.isEmpty() == false);
+  }
+}
 
-	//You won't do all the tests in main; that's what your Test class will be for
-	//Example:
-	//TestSuite myTester;
-	//myTester.runTests();
-
-	std::cout << "Running...\nAnd we're done.\nGoodbye.\n";
-	
-	return (0);
+//testing size function
+TEST_CASE("Size reports the size of the list", "[List]")
+{
+  LinkedListOfInts list;
+  SECTION("list is empty")
+  {
+    CHECK(list.size() == 0);
+  }
+  SECTION("list has one item")
+  {
+    list.addFront(1);
+    auto vector = list.toVector();
+    CHECK(list.size() == 1);
+  }
+  SECTION("list has multiple items")
+  {
+    list.addFront(1);
+    list.addFront(2);
+    list.addBack(3);
+    auto vector = list.toVector();
+    CHECK(list.size() == 3);
+  }
 
 }
 
+//testing search function
+TEST_CASE("searching through a list", "[List]")
+{
+  LinkedListOfInts list;
+  SECTION("empty list")
+  {
+    CHECK(list.search(1) == false);
+  }
+  SECTION("list has multiple items")
+  {
+    list.addFront(1);
+    list.addBack(2);
+    CHECK(list.search(1) == true);
+  }
+}
+
+//testing addBack function
+TEST_CASE("adding item to back of list", "[List]")
+{
+  LinkedListOfInts list;
+  list.addBack(1);
+  list.addBack(2);
+  list.addBack(3);
+  auto vector = list.toVector();
+	CHECK(vector.size() == 3);
+	CHECK(vector[0] == 1);
+	CHECK(vector[1] == 2);
+	CHECK(vector[2] == 3);
+}
+
+//testing addFront
+TEST_CASE("adding item to front of list", "[List]")
+{
+  LinkedListOfInts list;
+  list.addFront(1);
+  list.addFront(2);
+  list.addFront(3);
+  auto vector = list.toVector();
+  CHECK(vector.size() == 3);
+  CHECK(vector[0] == 3);
+	CHECK(vector[1] == 2);
+	CHECK(vector[2] == 1);
+}
+
+//testing removeBack
+TEST_CASE("removing item from back of list", "[List]")
+{
+  LinkedListOfInts list;
+  list.addFront(1);
+  list.addFront(2);
+  list.addFront(3);
+  auto vector = list.toVector();
+  CHECK(vector.size() == 3);
+  CHECK(vector[0] == 3);
+  CHECK(vector[1] == 2);
+  CHECK(vector[2] == 1);
+  list.removeBack();
+  CHECK(vector.size() == 2);
+  CHECK(vector[0] == 3);
+  CHECK(vector[1] == 2);
+  list.removeBack();
+  CHECK(vector.size() == 1);
+  CHECK(vector[0] == 3);
+}
+
+//testing removeFront
+TEST_CASE("removing item from front of list", "[List]")
+{
+  LinkedListOfInts list;
+  list.addFront(3);
+  list.addFront(2);
+  list.addFront(1);
+  auto vector = list.toVector();
+  CHECK(vector.size() == 3);
+  CHECK(vector[0] == 1);
+  CHECK(vector[1] == 2);
+  CHECK(vector[2] == 3);
+  list.removeBack();
+  CHECK(vector.size() == 2);
+  CHECK(vector[0] == 1);
+  CHECK(vector[1] == 2);
+  list.removeBack();
+  CHECK(vector.size() == 1);
+  CHECK(vector[0] == 1);
+}
